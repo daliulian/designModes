52d5953baf9c24ea82e762cfa1eb95d44e66279e
liyoulian <liyoulian@73go.com>
2018-09-18 16:21:43 +0800

    抽象工厂

--- src/AbstractFactory/AbstractFactory.java
+++ src/AbstractFactory/AbstractFactory.java
@@ -11,8 +11,12 @@ package AbstractFactory;
  * ***********************************************************
  */
 public class AbstractFactory {
-    public void code(String name){
-      Food food=new FactoryA().get();
-      food=new FactoryB().get();
-    }
+
+  /**
+   * 抽象工厂的灵活性就体现在这里，无需改动原有的代码，毕竟对于客户端来说，静态工厂模式在不改动StaticFactory类的代码时无法新增产品 ，如果采用了抽象工厂模式，就可以轻松的新增拓展类
+   */
+  public void code(String name) {
+    Food food = new FactoryA().get();
+    food = new FactoryB().get();
+  }
 }
